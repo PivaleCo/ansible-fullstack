b032bfb50761013f200715d298cf026f0de7eccb
Barry Fisher <barry@reallifedesign.co.uk>
2015-11-17 01:43:43 +0000

    Cleanup and simply of logic. Fixed virtualmin-prod references and made all paths to scripts relative

--- roles/virtualmin/scripts/disable_virtualmin_features.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Disable all virtualmin features (awstats, webalizer, mailman) but not htpasswd
-sed -i "s|^plugins=.*|plugins=virtualmin-htpasswd|g" /etc/webmin/virtual-server/config
--- roles/virtualmin/scripts/set_webmin_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-WEBMIN_VERSION=$(cat /etc/webmin/webmin/config | grep last_version_number | cut -d "=" -f2)
-sed -i "s|server=MiniServ/.*|server=MiniServ/$WEBMIN_VERSION|g" /etc/webmin/miniserv.conf
