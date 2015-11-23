62cf61d2d7536103e5b6ccbff4231ebecefb9dc0
Barry Fisher <barry@reallifedesign.co.uk>
2015-11-23 17:56:10 +0000

    Corrected line ending issue in virtualmin install script

--- roles/virtualmin/scripts/virtualmin-install.sh
+++ roles/virtualmin/scripts/virtualmin-install.sh
@@ -2,6 +2,8 @@
 
 # Get the install file
 wget http://software.virtualmin.com/gpl/scripts/install.sh -O /root/install.sh
+# Make the install file executable
+chmod u+x /root/install.sh
 # Run the installer
 /bin/bash /root/install.sh -y
 # Remove the install file
