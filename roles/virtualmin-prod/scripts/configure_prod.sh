6b90b28eb789c1cf9bd024778fb403ffa691d71a
Barry Fisher <barry@reallifedesign.co.uk>
2015-11-25 00:30:09 +0000

    Added configure prod task

--- roles/virtualmin-prod/scripts/configure_prod.sh
+++ roles/virtualmin-prod/scripts/configure_prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone git://github.com/reallifedesign/dotfiles ~/.dotfiles
-cd ~/.dotfiles
+git clone git://github.com/reallifedesign/dotfiles /home/prod/.dotfiles
+cd /home/prod/.dotfiles
 rake -f Rakefile.auto install
+exit 0
